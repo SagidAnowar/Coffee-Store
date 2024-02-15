c738b9a16d9b8680f00c736d0380b2e2f26875fb
Sagid Anowar <sagidanowar@gmail.com>
2024-02-14 19:00:13 -0500

    editing index and app jsx and scss

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import './home.scss'
+import { ReactComponent as TextDivider } from '../../Assets/text-divider.svg';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
